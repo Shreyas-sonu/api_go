3edd0280ddaa9e49ca02da1aaaed829ae506772d
Shreyas <sonureigns007@gmail.com>
2023-10-24 17:18:54 +0530

    auth apis added

---
+++ routes/authRoutes.go
@@ -0,0 +1,25 @@
+package routes
+
+import (
+	"simpleapi/controllers"
+	"simpleapi/middleware"
+
+	"github.com/gin-gonic/gin"
+)
+
+type AuthRouteController struct {
+	authController controllers.AuthController
+}
+
+func NewAuthRouteController(authController controllers.AuthController) AuthRouteController {
+	return AuthRouteController{authController}
+}
+
+func (rc *AuthRouteController) AuthRoute(rg *gin.RouterGroup) {
+	router := rg.Group("/auth")
+
+	router.POST("/register", rc.authController.SignUpUser)
+	router.POST("/login", rc.authController.SignInUser)
+	router.GET("/refresh", rc.authController.RefreshAccessToken)
+	router.GET("/logout", middleware.DeserializeUser(), rc.authController.LogoutUser)
+}

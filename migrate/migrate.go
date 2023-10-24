88f6e04ff4d4328f6747cc2eee38845a65dac9b2
Shreyas <sonureigns007@gmail.com>
2023-10-24 17:22:58 +0530

    migrations added

---
+++ migrate/migrate.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+	"log"
+
+	"api_go/initializers"
+	"api_go/models"
+)
+
+func init() {
+	config, err := initializers.LoadConfig(".")
+	if err != nil {
+		log.Fatal("🚀 Could not load environment variables", err)
+	}
+
+	initializers.ConnectDB(&config)
+}
+
+func main() {
+	initializers.DB.AutoMigrate(&models.User{}, &models.Post{})
+	fmt.Println("👍 Migration complete")
+}

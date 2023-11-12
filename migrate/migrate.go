2d51dd190a9a32bb663f431bd16f8a2e7217e2e2
Shreyas <sonureigns007@gmail.com>
2023-11-12 22:59:58 +0530

    middle ware altered

--- migrate/migrate.go
+++ migrate/migrate.go
@@ -1,23 +1,23 @@
-package main
+	package main
 
-import (
-	"fmt"
-	"log"
+	import (
+		"fmt"
+		"log"
 
-	"api_go/initializers"
-	"api_go/models"
-)
+		"api_go/initializers"
+		"api_go/models"
+	)
 
-func init() {
-	config, err := initializers.LoadConfig(".")
-	if err != nil {
-		log.Fatal("🚀 Could not load environment variables", err)
+	func init() {
+		config, err := initializers.LoadConfig(".")
+		if err != nil {
+			log.Fatal("🚀 Could not load environment variables", err)
+		}
+
+		initializers.ConnectDB(&config)
 	}
 
-	initializers.ConnectDB(&config)
-}
-
-func main() {
-	initializers.DB.AutoMigrate(&models.User{}, &models.Post{})
-	fmt.Println("👍 Migration complete")
-}
+	func main() {
+		initializers.DB.AutoMigrate(&models.User{}, &models.Post{})
+		fmt.Println("👍 Migration complete")
+	}

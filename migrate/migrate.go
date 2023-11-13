f62d206ae5729ecd1ce0f1b7234799df8a93ae3b
Shreyas <sonureigns007@gmail.com>
2023-11-13 23:36:48 +0530

    migrate

--- migrate/migrate.go
+++ migrate/migrate.go
@@ -1,23 +1,22 @@
-	package main
+package main
 
-	import (
-		"fmt"
-		"log"
-
-		"api_go/initializers"
-		"api_go/models"
+import (
+	"fmt"
+	"log"
+	"api_go/initializers"
+	"api_go/models"
 	)
 
-	func init() {
-		config, err := initializers.LoadConfig(".")
-		if err != nil {
-			log.Fatal("🚀 Could not load environment variables", err)
-		}
-
-		initializers.ConnectDB(&config)
+func init() {
+	config, err := initializers.LoadConfig(".")
+	if err != nil {
+		log.Fatal("🚀 Could not load environment variables", err)
 	}
 
-	func main() {
-		initializers.DB.AutoMigrate(&models.User{}, &models.Post{})
-		fmt.Println("👍 Migration complete")
-	}
+	initializers.ConnectDB(&config)
+}
+
+func main() {
+	initializers.DB.AutoMigrate(&models.User{}, &models.Post{})
+	fmt.Println("👍 Migration complete")
+}
